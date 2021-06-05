96964510fcb8add747f2580ed3e38f080659bf20
zaidalasfar97 <z.alasfar@hotmail.com>
2021-06-06 00:11:25 +0300

    not

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from 'react';
+import About from './components/about/about.js';
 function App() {
-  return <div></div>;
+  return (
+
+    <div>
+      <About />
+    </div>
+  )
 }
 
 export default App;
