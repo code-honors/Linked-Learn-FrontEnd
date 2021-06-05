0e11265a4dc2ea486d3a190d7f28a1fedc42dafe
ahmadfrijat <ahmadalfrijat7@gmail.com>
2021-06-06 01:49:56 +0300

    tracher-profile

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import store from './store';
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <App />
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,20 @@ import { Switch, Route } from 'react-router-dom';
 import Sign from '../components/sign/sign-in'
 import NotFound from '../components/NotFound';
 import Home from '../components/home/home';
+import Courses from '../components/teacherProfile/courses';
+import curriculum from '../components/teacherProfile/curriculum';
+
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/signin" component={Sign} />
       <Route exact path="/" component={Home} />
+      <Route exact path="/teacher/profile" component={ Courses} />
+      <Route exact path="/teacher/profile/curriculum" component={ curriculum} />
       <Route component={NotFound} />
+
+
     </Switch>
   );
 }
