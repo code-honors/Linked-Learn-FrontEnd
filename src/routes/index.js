1be5de7b7926ca1f5a1d7cdb987fc07917cc602a
rubabanat <rubabanat1996@gmail.com>
2021-06-05 17:31:25 +0300

    homePage

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import { Switch, Route } from 'react-router-dom';
 import NotFound from '../components/NotFound';
-import Home from '../components/Home';
+import Home from '../components/home/home';
 
 export default function Routes() {
   return (
