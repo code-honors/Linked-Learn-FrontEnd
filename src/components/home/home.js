9deaff595c941f0e2698505670856c459861b4f4
rubabanat <rubabanat1996@gmail.com>
2021-06-08 02:20:18 +0300

    dashboard done

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -29,25 +29,6 @@ function Home() {
   return (
 <>
 {/* /* react-page-scroller */ }
- {/* <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-        width: '5rem',
-        margin: '0 auto',
-      }}
-    >
-      <div style={{ cursor: 'pointer' }} onClick={() => handlePage(0)}>
-        1
-      </div>
-      <div style={{ cursor: 'pointer' }} onClick={() => handlePage(1)}>
-        2
-      </div>
-      <div style={{ cursor: 'pointer' }} onClick={() => handlePage(2)}>
-        3
-      </div>
-    </div>  */}
 <div
   style={{
     display: 'flex',
@@ -71,89 +52,8 @@ function Home() {
 </div> 
 {/* end of react-page-scroller  */ }
  </>
-    // @splidejs/react-splide
-    // <>
-      /* <ReactScrollWheelHandler
-        upHandler={(e) => console.log("scroll up")}
-        downHandler={(e) => console.log("scroll down")}
-        style={{
-          width: "100%",
-          height: "100vh",
-          transition: "backgroundColor .5s ease-out",
-        }}
-      >
-        <Splide
-          options={{
-            type: 'fade',
-            direction: 'ttb',
-            height: '100vh',  
-            updateOnMove:true,
-            arrows: false,
-            autoplay: true,
-            interval: 5000,
-            pauseOnHover: false,
-          }}
-        >
-          <SplideSlide>
-            <Main />
-          </SplideSlide>
-          <SplideSlide>
-            <About />
-          </SplideSlide>
-          <SplideSlide>
-            <Contact />
-          </SplideSlide>
-          <SplideSlide>
-            <Sign />
-          </SplideSlide>
-        </Splide>
-      </ReactScrollWheelHandler>
-    </> */
-    // end of @splidejs/react-splide
+  
   );
 }
 
 export default Home;
-// <>
-{/* /* react-page-scroller */ }
-{/* <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-        width: '5rem',
-        margin: '0 auto',
-      }}
-    >
-      <div style={{ cursor: 'pointer' }} onClick={() => handlePage(0)}>
-        1
-      </div>
-      <div style={{ cursor: 'pointer' }} onClick={() => handlePage(1)}>
-        2
-      </div>
-      <div style={{ cursor: 'pointer' }} onClick={() => handlePage(2)}>
-        3
-      </div>
-    </div> */}
-// <div
-//   style={{
-//     display: 'flex',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     flexDirection: 'column',
-//   }}
-// >
-//   <ReactPageScroller
-//     pageOnChange={handlePage}
-//     containerWidth={window.innerWidth *1}
-//     containerHeight={window.innerHeight * 1}
-//     customPageNumber={page}
-//     onBeforePageScroll={(e) => handleUnavailable(e)}
-//   >
-//     <Main />
-//     <About />
-//     <Contact />
-//   </ReactPageScroller>
-// </div> 
-{/* end of react-page-scroller  */ }
-  //  </>
