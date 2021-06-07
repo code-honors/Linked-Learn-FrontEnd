1d2ca11d86fed35d187d9bdee20bcec1af5a81e1
ahmadfrijat <ahmadalfrijat7@gmail.com>
2021-06-08 00:18:58 +0300

    fix

--- src/components/teacherProfile/curriculum.js
+++ src/components/teacherProfile/curriculum.js
@@ -10,10 +10,10 @@ function curriculum() {
 
     return (
         <>
-            <Navbar/>
-        <section className='products'>
+            <Navbar />
+            <section className='products'>
 
-                <Figure style={{marginRight:"20px"}}>
+                <Figure style={{ marginRight: "20px" }}>
                     <Figure.Image
                         width={300}
                         height={400}
@@ -23,49 +23,49 @@ function curriculum() {
                     <Card.Title>Linda Mailss</Card.Title>
                     <Card.Text>Special Assistant</Card.Text>
                     <Figure.Caption>
-                    Lorem ipsum dolor sit amet, consec  incididunt 
+                        Lorem ipsum dolor sit amet, consec  incididunt
                    </Figure.Caption>
-                   <Figure.Caption>
-                   ut labore.tetur adipiscing, sed do eiusmod  </Figure.Caption>
-                   <Figure.Caption>
-                   tempor incididunt ut labore.
+                    <Figure.Caption>
+                        ut labore.tetur adipiscing, sed do eiusmod  </Figure.Caption>
+                    <Figure.Caption>
+                        tempor incididunt ut labore.
                    </Figure.Caption>
-                   <SocialIcon style={{ height: 25, width: 25 }} bgColor="white" fgColor="black" url="https://linkedin.com/in/jaketrent" />
-                   <SocialIcon style={{ height: 25, width: 25 }} bgColor="white" fgColor="black" url="https://twitter.com/in/jaketrent"  />
-                   <SocialIcon style={{ height: 25, width: 25 }} bgColor="white" fgColor="black" url="https://gogleplus.com/in/jaketrent"  />
+                    <SocialIcon style={{ height: 25, width: 25 }} bgColor="white" fgColor="black" url="https://linkedin.com/in/jaketrent" />
+                    <SocialIcon style={{ height: 25, width: 25 }} bgColor="white" fgColor="black" url="https://twitter.com/in/jaketrent" />
+                    <SocialIcon style={{ height: 25, width: 25 }} bgColor="white" fgColor="black" url="https://gogleplus.com/in/jaketrent" />
 
                 </Figure>
                 <div >
-                <nav >
-                <ul>
-                  <li><NavLink style={{color:"#ff1949"}} to='/teacher/profile'>Courses</NavLink></li>
-                  <li ><NavLink style={{color:"#ff1949"}} to='/teacher/profile/curriculum'>Curriculum</NavLink></li>
-                </ul>
-                </nav>
-                <Card className="pDiv"> 
-                    <Card.Title>Hello. This is my story.</Card.Title>
-                    <Figure.Caption>Lorem Ipsn gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quise bibendum auci elit consequat ipsutis sem nibh id elit. Duis sed odio sit amet nih vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auct a ornare odio. Sed non mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosquad litora torquent per conubia nostra peri.</Figure.Caption>
-                    <Figure.Caption>Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non maur is vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora to rquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit ed ut imperdiet nisi. Proin condimentum fermentum nunc uorem Ipsn gravi daei.</Figure.Caption>
+                    <nav >
+                        <ul>
+                            <li><NavLink style={{ color: "#ff1949" }} to='/teacher/profile'>Courses</NavLink></li>
+                            <li ><NavLink style={{ color: "#ff1949" }} to='/teacher/profile/curriculum'>Curriculum</NavLink></li>
+                        </ul>
+                    </nav>
+                    <Card className="card1" style={{width:"668px",padding:"15px",margin:"15px"}}>
+                        <Card.Title>Hello. This is my story.</Card.Title>
+                        <Figure.Caption>Lorem Ipsn gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quise bibendum auci elit consequat ipsutis sem nibh id elit. Duis sed odio sit amet nih vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auct a ornare odio. Sed non mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosquad litora torquent per conubia nostra peri.</Figure.Caption>
+                        <Figure.Caption>Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non maur is vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora to rquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit ed ut imperdiet nisi. Proin condimentum fermentum nunc uorem Ipsn gravi daei.</Figure.Caption>
                     </Card>
-                    
-                    <Card.Title style={{ marginLeft:"18px"}}>Download PDF</Card.Title>
-                    <Figure.Image
-                        style={{ margin:"15px", marginLeft:"18px"}}
-                        width={200}
-                        height={200}
-                        alt="171x180"
-                        src="https://academist.qodeinteractive.com/wp-content/uploads/2018/07/Certificate.jpg"
-                    />
-                     <Figure.Image
-                        style={{float:"left", margin:"10px"}}
-                        width={200}
-                        height={200}
-                        alt="171x180"
-                        src="https://academist.qodeinteractive.com/wp-content/uploads/2018/07/Certificate.jpg"
-                    />
-                    </div>
 
-                    <Card className="p2Div"> 
+                    <Card.Title style={{ marginLeft: "18px" }}>Download PDF</Card.Title>
+                    <Figure.Image
+                        style={{ margin: "15px", marginLeft: "18px" }}
+                        width={200}
+                        height={200}
+                        alt="171x180"
+                        src="https://academist.qodeinteractive.com/wp-content/uploads/2018/07/Certificate.jpg"
+                    />
+                    <Figure.Image
+                        style={{ float: "left", margin: "10px" }}
+                        width={200}
+                        height={200}
+                        alt="171x180"
+                        src="https://academist.qodeinteractive.com/wp-content/uploads/2018/07/Certificate.jpg"
+                    />
+                </div>
+
+                <Card className="card2" style={{width:"256px",padding:"15px",margin:"15px",marginTop:"59px"}}>
                     <Card.Title>My education</Card.Title>
                     <h6>Harvard University</h6>
                     <Figure.Caption>MBA from Harvard Business School</Figure.Caption>
@@ -79,14 +79,14 @@ function curriculum() {
                     <Figure.Caption>BBA from University of Michigan</Figure.Caption>
                     <h6>Google</h6>
                     <Figure.Caption>MBA from Harvard Business School</Figure.Caption>
-                    </Card>
-              
-                   
+                </Card>
 
-              
-            
-        </section>
-        <Footer/>
+
+
+
+
+            </section>
+            <Footer />
 
         </>
     )
